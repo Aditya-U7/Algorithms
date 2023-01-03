3a8b6d1aaa4027e9fd591e497583ec4981e2425d
Aditya-U7 <66748266+Aditya-U7@users.noreply.github.com>
2023-01-03 18:17:28 +0530

    Add files via upload

---
+++ bubble_sort.py
@@ -0,0 +1,20 @@
+    
+def bubble_sort(arr):
+    for i in range(0,j-1):
+        for k in range(i+1,j):
+            if arr[i]>arr[k]:
+               arr[i],arr[k]=arr[k],arr[i]  # A function should be defined before it is called. 
+                                            #The body of the function can be defined anywhere in the program.
+    return(arr)
+
+siz=input("Enter the no of elements:")
+arr=list(input().split())
+j=0
+for i in arr:
+    arr[j]=int(i)
+    j=j+1
+    
+    
+print(bubble_sort(arr))    
+     
+
