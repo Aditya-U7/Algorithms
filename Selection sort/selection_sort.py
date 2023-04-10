3f99060410b3f270ce3f37a662ab8be3d23e5d25
Aditya-U7 <66748266+Aditya-U7@users.noreply.github.com>
2023-04-10 21:09:47 +0530

    Add files via upload

---
+++ Selection sort/selection_sort.py
@@ -0,0 +1,28 @@
+arr=[]
+
+def selection_sort():
+ for i in range(len(arr)-1):
+  for j in range(i,len(arr),1):
+   if arr[i]>arr[j]:
+       arr[i],arr[j]=arr[j],arr[i]
+ 
+
+def accept_user_input():
+ global arr
+ print("Enter the elements: ",end="\n")
+ arr=list(input().split())      
+ arr=[int(i) for i in arr]
+ 
+def print_sorted_list():
+ print("Elements after sorting by Selection sort:",end="\n")
+ for item in arr:
+  print(item,end=' ')
+ print("\n")
+
+
+accept_user_input()
+
+selection_sort()
+
+print_sorted_list() 
+     
