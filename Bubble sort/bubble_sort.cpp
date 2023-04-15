63d67ed1fe9d3d55f49d644ab50ee09b52d18a59
Aditya-U7 <66748266+Aditya-U7@users.noreply.github.com>
2023-04-15 20:06:23 +0530

    Update bubble_sort.cpp

--- Bubble sort/bubble_sort.cpp
+++ Bubble sort/bubble_sort.cpp
@@ -5,15 +5,15 @@ using namespace std;
 void bubble_sort(vector<int>& rarray)
 {
 	int tmp;
-	for(int i=0;i<rarray.size()-1;i++)
+	for ( int i=0; i<rarray.size()-1 ; i++)
 	{
-		for(int j=0;j<rarray.size()-i-1;j++)
+		for ( int j=0; j<rarray.size()-i-1; j++)
 		{
-			if(rarray[j]>rarray[j+1])
+			if (rarray[j] > rarray[j+1])
 			{
-				tmp=rarray[j];
-				rarray[j]=rarray[j+1];
-				rarray[j+1]=tmp;
+				tmp = rarray[j];
+				rarray[j] = rarray[j+1];
+				rarray[j+1] = tmp;
 			}
 		}
 	}
@@ -21,20 +21,23 @@ void bubble_sort(vector<int>& rarray)
 
 int main()
 {
-	int size,element;
+	int size;
+	int element;
 	vector<int> darray;
 	cout<<"Do enter the total count of elements:"<<endl;
 	cin>>size;
 	cout<<"Enter the elements: "<<endl;
-	for(int i=0;i<size;i++)
+	for ( int i=0; i<size ;i++ )
 	{
 		cin>>element;
 		darray.push_back(element);
 	}
 	cout<<endl;
+	
 	bubble_sort(darray);
+	
 	cout<<"Elements after bubble_sort: "<<endl;
-	for(auto& vr: darray)
+	for ( auto& vr: darray )
 		cout<<vr<<" ";
 	cout<<endl;	
 	return 0;
