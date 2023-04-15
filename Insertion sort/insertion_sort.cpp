804357a38afa37da9a7af975808b532c4ca094fe
Aditya-U7 <66748266+Aditya-U7@users.noreply.github.com>
2023-04-15 20:08:35 +0530

    Update insertion_sort.cpp

--- Insertion sort/insertion_sort.cpp
+++ Insertion sort/insertion_sort.cpp
@@ -4,15 +4,15 @@ using namespace std;
 void  insertion_sort(vector<int>& array)
 { 
 	int tmp;          
-	for(int i=1;i<array.size();i++)
+	for ( int i = 1; i < array.size(); i++ )
 	{
-		for(int j=i;j>0;j--)
+		for ( int j=i; j>0 ; j-- )
 		{
-			if(array[j]<array[j-1])
+			if ( array[j] < array[j-1] )
 			{
-				tmp=array[j];
-				array[j]=array[j-1];
-				array[j-1]=tmp;
+				tmp = array[j];
+				array[j] = array[j-1];
+				array[j-1] = tmp;
 
 
 
@@ -22,12 +22,13 @@ void  insertion_sort(vector<int>& array)
 }
 int main()
 {
-	int size,element;
+	int size;
+	int element;
 	vector<int> darray;
 	cout<<"Do enter the total count of elements:"<<endl;
 	cin>>size;
 	cout<<"Enter the elements: "<<endl;
-	for(int i=0;i<size;i++)
+	for ( int i = 0; i<size ; i++)
 	{
 		cin>>element;
 		darray.push_back(element);
@@ -35,7 +36,7 @@ int main()
 	cout<<endl;
 	insertion_sort(darray);
 	cout<<"Elements after insertion_sort: "<<endl;
-	for(auto& vr: darray)
+	for ( auto& vr: darray )
 		cout<<vr<<endl;
 	cout<<endl;	
 	return 0;
