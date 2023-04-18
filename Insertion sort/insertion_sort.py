826d767dd84394eab22f58465ef1bed54a5fcab9
Aditya-U7 <66748266+Aditya-U7@users.noreply.github.com>
2023-04-18 17:31:13 +0530

    Update insertion_sort.py

--- Insertion sort/insertion_sort.py
+++ Insertion sort/insertion_sort.py
@@ -1,27 +1,31 @@
-arr=[] 
+arr = []
 
-def insertion_sort():
- for i in range(len(arr)):
-  for j in range(i,0,-1):
-    if arr[j]<arr[j-1]:
-       arr[j],arr[j-1]=arr[j-1],arr[j]
-        
+
+def insertion_sort(arr):
+    for i in range(len(arr)):
+        for j in range(i, 0, -1):
+            if arr[j] < arr[j - 1]:
+                arr[j], arr[j - 1] = arr[j - 1], arr[j]
+
+
 def accept_user_input():
- global arr
- print("Enter the elements: ",end="\n")
- arr=list(input().split())      
- arr=[int(i) for i in arr]
- 
+    global arr
+    print("Enter the elements: ", end="\n")
+    arr = list(input().split())
+    arr = [int(i) for i in arr]
+
+
 def print_sorted_list():
- print("Elements after sorting by Insertion sort:",end="\n")
- for item in arr:
-  print(item,end=' ')
- print("\n") 
+    print("Elements after sorting by Insertion sort:", end="\n")
+    for item in arr:
+        print(item, end=" ")
+    print("\n")
 
 
 accept_user_input()
 
-insertion_sort()
+insertion_sort(arr)
 
-print_sorted_list() 
+print_sorted_list()
+ 
  
