d0e64f6a37b01c002863552c11cfcb8bda917c6c
Aditya-U7 <66748266+Aditya-U7@users.noreply.github.com>
2023-04-18 17:30:05 +0530

    Update bubble_sort.py

--- Bubble sort/bubble_sort.py
+++ Bubble sort/bubble_sort.py
@@ -1,31 +1,31 @@
-arr=[]    
+arr = []
 
-def bubble_sort():
-    
-    for i in range(0,len(arr)-1):
-        for j in range(0,len(arr)-i-1):
-            if arr[j]>arr[j+1]:
-               arr[j],arr[j+1]=arr[j+1],arr[j]  
-    
+
+def bubble_sort(arr):
+
+    for i in range(0, len(arr) - 1):
+        for j in range(0, len(arr) - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
 
 def accept_user_input():
- global arr
- print("Enter the elements: ",end="\n")
- arr=list(input().split())      
- arr=[int(i) for i in arr]
- 
+    global arr
+    print("Enter the elements: ", end="\n")
+    arr = list(input().split())
+    arr = [int(i) for i in arr]
+
 
 def print_sorted_list():
- print("Elements after sorting by Bubble sort:",end="\n")
- for item in arr:
-  print(item,end=' ')
- print("\n")
+    print("Elements after sorting by Bubble sort:", end="\n")
+    for item in arr:
+        print(item, end=" ")
+    print("\n")
 
 
 accept_user_input()
 
-bubble_sort()
+bubble_sort(arr)
 
-print_sorted_list()  
+print_sorted_list()
      
