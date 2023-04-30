ee9a0ae3c9e81f441631527738b72bc1385a9a08
Aditya-U7 <66748266+Aditya-U7@users.noreply.github.com>
2023-05-01 01:27:07 +0530

    Add files via upload

--- Graph_traversal_algorithms/Depth_First_Search/dfs.cpp
+++ Graph_traversal_algorithms/Depth_First_Search/dfs.cpp
@@ -1,91 +1,109 @@
 #include <iostream>
 #include <vector>
 #include <stack>
-#include <queue>
-using namespace std;
 
-vector<vector<int>> vertices;
-stack<int> s;
-vector<bool> visited;
 
-void initialise(vector<bool>& v)
+void dfs(std::vector< std::vector<int>>& adjacency_list, int vertex, std::vector<bool>& visited)
 {
-	v.resize(vertices.size());
 
-	for (int i = 0; i <= vertices.size(); i++)
-		v[i] = false;
 
-}	// initialise ends
-
-void dfs(int vertex)
-{
 	visited[vertex] = true;
 
-	s.push(vertex);
-
-	for (auto& neighbour: vertices[vertex])
+	for (auto& neighbour: adjacency_list[vertex])
 	{
 		if (!visited[neighbour])
-			dfs(neighbour);
+			dfs(adjacency_list, neighbour, visited);
 	}
 
-	cout << s.top() << endl;
-	s.pop();
+	std::cout << vertex << std::endl;
 
 }
+
+
+void read_vertex_and_edge_count(int& v_count, int& e_count)
+{
+
+	std::cout << "Enter the no of vertices:" << std::endl;
+	std::cin >> v_count;
+	std::cout << "Enter the no of edges: " << std::endl;
+	std::cin >> e_count;
+
+}
+
+
+void read_the_graph_type(char& gt)
+{
+
+	std::cout << "Enter the graph type - directed[ d ] or undirected[ u ]:  ";
+	std::cin >> gt;
+
+}
+
 
 void check_vertex(int& v, int& vc)	// check_vertex starts
 {
 	while (v > vc || v < 0)
 	{
-		cout << "Invalid vertex no: " << v << endl;
-		cout << "Re enter the vertex: " << endl;
-		cin >> v;
+		std::cout << "Invalid vertex no: " << v << std::endl;
+		std::cout << "Re enter the vertex: " << std::endl;
+		std::cin >> v;
 	}
 }	// check_vertex ends
 
-int main()
-{
-	int start_vertex;
-	int vertex;
-	int v_count = 0;
-	int edges_count = 0;
-	char graph_type = 'd';
-	cout << "Enter the no of vertices:" << endl;
-	cin >> v_count;
-	vertices.resize(v_count + 1);
 
-	cout << "Enter the no of edges: " << endl;
-	cin >> edges_count;
+void read_the_edges(std::vector<std::vector<int>>& adj, int& ec, int& v_count, char& gt)
+{
 
 	int source;
 	int destination;
 
-	cout << "Enter the graph type - directed[ d ] or undirected[ u ]:  ";
-	cin >> graph_type;
 
-	cout << "\nEnter the edges: " << endl;
 
-	while (edges_count--)
+	std::cout << "\nEnter the edges: " << std::endl;
+
+	while (ec--)
 	{
-		cin >> source >> destination;
+		std::cin >> source >> destination;
 
 		check_vertex(source, v_count);
 		check_vertex(destination, v_count);
 
-		vertices[source].push_back(destination);
+		adj[source].push_back(destination);
 
-		if (graph_type == 'u')
-			vertices[destination].push_back(source);
+		if (gt == 'u')
+			adj[destination].push_back(source);
 	}
 
-	cout << "\nEnter the start vertex: ";
-	cin >> start_vertex;
-	initialise(visited);
-	
-	check_vertex(start_vertex, v_count)
-	
-	dfs(start_vertex);
+
+}
+
+
+int main()
+{
+
+	int start_vertex;
+	int vertex;
+	int vertices_count = 0;
+	int edges_count = 0;
+	char graph_type = 'd';
+
+	read_vertex_and_edge_count(vertices_count, edges_count);
+
+	std::vector< std::vector<int> > adjacency_list(vertices_count + 1);
+
+	read_the_graph_type(graph_type); 
+
+	read_the_edges(adjacency_list, edges_count, vertices_count, graph_type);
+
+	std::cout << "\nEnter the start vertex: ";
+	std::cin >> start_vertex;
+
+	check_vertex(start_vertex, vertices_count); 
+
+	std::vector<bool> visited(adjacency_list.size(), false);
+	visited[start_vertex] = true;
+
+	dfs(adjacency_list, start_vertex, visited);
 
 	return 0;
 }
