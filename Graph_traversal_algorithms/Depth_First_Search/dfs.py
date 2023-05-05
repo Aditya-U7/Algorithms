66fd2d320a66f4abab5009ad64c0f81fd3c9fbfc
Aditya-U7 <66748266+Aditya-U7@users.noreply.github.com>
2023-05-05 17:01:04 +0530

    Update dfs.py

--- Graph_traversal_algorithms/Depth_First_Search/dfs.py
+++ Graph_traversal_algorithms/Depth_First_Search/dfs.py
@@ -1,6 +1,3 @@
-adj_list = []
-visited = []
-
 def check_vertex(vertex, nov):
     
     while(1):
@@ -15,28 +12,43 @@ def check_vertex(vertex, nov):
             return vertex
 
 
-def initialise_visited(nov):
-    
-    
-    
-    for i in range(nov):     
-        visited.append(False)
-    
-    return visited
-    
+def read_edges(nov, noe, adj_list): 
 
-def dfs(vertex):
+    print("Enter the edges: ")
+
+    while (noe):
+
+        vertex = list(input().split())
+        source, destination = int(vertex[0]), int(vertex[1])     
+    
+        source = check_vertex(source, nov)
+        destination = check_vertex(destination, nov)
+     
+   
+        adj_list[source].append(destination)
+    
+        if graph_type == 'u':
+        
+            adj_list[destination].append(source)
+    
+        noe -= 1 
+           
+    return adj_list
+
+
+def dfs(adj_list, vertex, visited):
     
     visited[vertex] = True
     
-    for neighbour in  adj_list[vertex]:
+    for neighbour in adj_list[vertex]:
         
         if not visited[neighbour]:
             
-            dfs(neighbour)    
+            dfs(adj_list, neighbour, visited)    
     
     print(vertex)
         
+########################################################
 
 no_of_vertices = int(input("Enter the no of vertices: "))
 
@@ -46,30 +58,12 @@ no_of_edges = int(input("Enter the no of edges: "))
 
 graph_type = input("Enter the graph type -  undirected [ u ] or directed [ d ]: ")
 
-print("Enter the edges: ")
+adj_list = read_edges(no_of_vertices, no_of_edges, adj_list)
 
-while (no_of_edges):
-
-    vertex = list(input().split())
-    source, destination = int(vertex[0]), int(vertex[1])     
-    
-    source = check_vertex(source, no_of_vertices)
-    destination = check_vertex(destination, no_of_vertices)
-    
-   
-    adj_list[source].append(destination)
-    
-    if graph_type == 'u':
-        
-        adj_list[destination].append(source)
-    
-    no_of_edges -=1
-
-     
 start_vertex = int(input("Enter the start vertex: "))
 
 start_vertex = check_vertex(start_vertex, no_of_vertices)
 
-visited = initialise_visited(len(adj_list))       
+visited = [False for i in range(no_of_vertices)]       
 
-dfs(start_vertex)
+dfs(adj_list, start_vertex, visited)
