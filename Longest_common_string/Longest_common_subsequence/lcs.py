5a86a81d970d5475f78f28c8a717de3491ba71d9
Aditya-U7 <66748266+Aditya-U7@users.noreply.github.com>
2023-05-30 04:48:39 +0530

    Update lcs.py

--- Longest_common_string/Longest_common_subsequence/lcs.py
+++ Longest_common_string/Longest_common_subsequence/lcs.py
@@ -3,21 +3,21 @@ def lcs(str1, str2):
     r = len(str1)
     c = len(str2)
     
-    mat = [[0 for j in range(c+1)] for i in range(r+1)]
+    mat = [[0 for j in range(c + 1)] for i in range(r + 1)]
     
     max_len = 0
     
-    for i in range(c-1, -1, -1):
+    for i in range(c - 1, -1, -1):
         
-        for j in range(r-1, -1, -1):
+        for j in range(r - 1, -1, -1):
              
              if str1[j] == str2[i]:
                  
-                 mat[j][i] = mat[j+1][i+1] + 1
+                 mat[j][i] = mat[j + 1][i + 1] + 1
              
              else:
              
-                 mat[j][i] = max(mat[j][i+1], mat[j+1][i])     
+                 mat[j][i] = max(mat[j][i + 1], mat[j + 1][i])     
              
              if mat[j][i] > max_len:
                
